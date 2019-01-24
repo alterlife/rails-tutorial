d5c6fb90a8dc31cd65c5548b9df64d93b4341426
Jeevan John <jeevan@Jeevans-MacBook-Pro.local>
2019-01-24 09:02:13 +0530

    Fix error caused by microposts being empty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,7 +65,6 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
-      @content = @user.microposts.first.content
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
