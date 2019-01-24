644ee9a98796782b5b6e5ec0a8eb33d6bdc1ff92
Jeevan John <jeevan@Jeevans-MacBook-Pro.local>
2019-01-24 08:44:44 +0530

    Add microposts list to the users page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      @content = @user.microposts.first.content
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
